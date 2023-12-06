0eed606b933d2ee4e6a631918bce0f1c8ff14de1
sub-acc-ni-carlo <johncarloevasco55@gmail.com>
2023-12-06 15:35:32 +0800

    added a comment

--- sprite.py
+++ sprite.py
@@ -1,4 +1,8 @@
 class Sprite:
+    
+    """
+    This class helps managing the animation of a character. 
+    """
 
     def __init__(self, group_name=None, animation=[]):
         self.group_name = group_name
